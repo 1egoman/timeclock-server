78821dec101d0aebc0bfeaf1952a324a9bb0bd55
1egoman <rsg1egoman@gmail.com>
2016-02-27 22:12:53 -0500

    add invoice back in, front and center

--- public/js/react/components/nav.js
+++ public/js/react/components/nav.js
@@ -5,10 +5,28 @@ export const NavComponent = ({title}) => {
   return <nav className="navbar">
     <div className="container-fluid">
       <div className="navbar-header">
-        <a className="navbar-brand" href="/">
-          {title}
-        </a>
+        <button
+          type="button"
+          className="navbar-toggle collapsed"
+          data-toggle="collapse"
+          data-target="#bs-example-navbar-collapse-1"
+          aria-expanded="false"
+        >
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a className="navbar-brand" href="/">{title}</a>
       </div>
+
+      <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <ul className="nav navbar-nav">
+          <li><a href="/features">Features</a></li>
+          <li><a href="/pricing">Pricing</a></li>
+        </ul>
+      </div>
+
     </div>
   </nav>;
 }
