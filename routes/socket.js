bb4e742a8b8524d74b22151ad1e431b72364d699
1egoman <rsg1egoman@gmail.com>
2016-03-04 20:48:46 -0500

    extract common routes from socket.js into own files

--- routes/socket.js
+++ routes/socket.js
@@ -5,7 +5,21 @@ const required_repo = require("../lib/repo"),
       _ = require("underscore"),
       user_model = require("../lib/models/user"),
       uuid = require("uuid"),
+
+      getTimecard = require("../lib/events/getTimecard"),
+      importRepo = require("../lib/events/importRepo"),
+      getBranches = require("../lib/events/getBranches"),
+      resetToken = require("../lib/events/resetToken"),
+      changeSetting = require("../lib/events/changeSetting"),
+
       TIMECARD_PAGE_LENGTH = 20; // the amount of times that are returned per request,
+
+function sendError(err) {
+  socket.emit("action", {
+    type: "server/ERROR",
+    error: typeof err.message === "string" ? err.message : err,
+  });
+}
 
 module.exports = function(socket, use_repo, inject_user_model) {
   let repo = use_repo || required_repo;
@@ -42,141 +56,50 @@ module.exports = function(socket, use_repo, inject_user_model) {
 
     // import a repo
     } else if (action.type === 'server/IMPORT_REPO') {
-      console.log("Import a repo!");
-
-      User.update({_id: socket.request.user}, {$push: {repos: action.repo}}, (err) => {
-        if (err) {
-          socket.emit("action", {
-            type: "server/ERROR",
-            error: err,
-          });
-        } else {
-          socket.emit("action", {
-            type: "server/PUT_REPO",
-            repo: action.repo,
-          });
-          socket.emit("action", {
-            type: "server/REPO_IMPORT",
-            repo: action.repo,
-          });
-        }
-      });
+      importRepo(action, socket).then((repo) => {
+        socket.emit("action", {
+          type: "server/PUT_REPO",
+          repo: action.repo,
+        });
+        socket.emit("action", {
+          type: "server/REPO_IMPORT",
+          repo: action.repo,
+        });
+      }, sendError);
 
     // get the branches for a repo
     } else if (action.type === 'server/GET_BRANCHES') {
-      repo.getBranchesForRepo({user: socket.request.user}, action.user, action.repo).then((branches) => {
+      getBranches(action, socket).then((branches) => {
         socket.emit("action", {
           type: "server/BRANCHES_FOR",
-          branches: branches.map((b) => b.name),
+          branches,
         });
-      }, (err) => {
-        socket.emit("action", {
-          type: "server/ERROR",
-          error: err,
-        });
-      });
+      }, sendError);
 
     // get the timecard for the specified repo
     } else if (action.type === 'server/GET_TIMECARD') {
-      repo.getFileFromRepo(action.user, action.repo, null, action.branch, socket.request.user).then((timecard) => {
-        if (card.assertIsCard(timecard)) {
-
-          // paginate according to the passed info
-          if (typeof action.page !== "undefined") {
-            let start = action.page * TIMECARD_PAGE_LENGTH;
-            // return them in reverse so they are sorted in
-            // reverse-chronological order
-            timecard.card = timecard.card.reverse().slice(start, start + TIMECARD_PAGE_LENGTH);
-          }
-
-          // get all users in the timecard
-          let all_users = timecard.card.map((day) => {
-            return day.times.map((t) => t.by);
-          });
-          let reduced_users = _.compact(_.uniq(_.flatten(all_users)));
-
-          // get all user details
-          async.map(reduced_users, (username, done) => {
-            repo.getUserMetaFor({user: socket.request.user}, username)
-            .then((user) => done(null, user))
-            .catch(done);
-          }, (err, user_models) => {
-
-            // send the timecard and the associated users
-            socket.emit("action", {
-              type: "server/TIMECARD",
-              user: action.user,
-              repo: action.repo,
-              branch: action.branch,
-              timecard: timecard,
-              users: user_models,
-
-              page: action.page,
-              canpaginateforward: timecard.card.length >= TIMECARD_PAGE_LENGTH,
-            });
-          });
-        } else {
-          // uhh, the timecard doesn't validate
-          socket.emit("action", {
-            type: "server/TIMECARD",
-            error: "Timecard isn't a timecard."
-          });
-        }
-      }, (err) => {
-        socket.emit("action", {
-          type: "server/ERROR",
-          error: err,
-        });
-      });
+      getTimecard(action, socket).then((timecard) => {
+        socket.emit("action", Object.assign({
+          type: "server/TIMECARD",
+        }, timecard));
+      }, sendError);
 
     // reset a badge token that is associated with a user
     } else if (action.type === 'server/RESET_TOKEN') {
-      User.findOne({username: action.username, badge_token: action.old_token}).exec((err, user) => {
-        if (err) {
-          socket.emit("action", {
-            type: "server/ERROR",
-            err: err,
-          });
-        } else if (user) {
-          user.badge_token = uuid.v4(); // generate new badge token
-          user.save((err) => {
-            if (err) {
-              socket.emit("action", {
-                type: "server/ERROR",
-                err: err,
-              });
-            } else {
-              socket.emit("action", {
-                type: "server/TOKEN_RESET",
-                user: User.sanitize(user),
-                badge_token: user.badge_token,
-              });
-            }
-          })
-        } else {
-          socket.emit("action", {
-            type: "server/ERROR",
-            err: "No user was found with that badge token.",
-          });
-        }
-      });
+      resetToken(action, socket).then((data) => {
+        socket.emit("action", Object.assign({
+          type: "server/TOKEN_RESET",
+        }, data));
+      }, sendError);
 
     // change the value of a sumset of settings within the user model
     } else if (action.type === 'server/CHANGE_SETTING') {
-      let new_settings = Object.assign(socket.request.user.settings, action.changes);
-      User.update({_id: socket.request.user._id}, {settings: new_settings}).exec((err) => {
-        if (err) {
-          socket.emit("action", {
-            type: "server/ERROR",
-            err: err,
-          });
-        } else {
-          socket.emit("action", {
-            type: "server/SETTING_CHANGED",
-            settings: new_settings,
-          });
-        }
-      });
+      changeSetting(action, socket).then((data) => {
+        socket.emit("action", {
+          type: "server/SETTING_CHANGED",
+          settings: data,
+        });
+      }, sendError);
 
     // when the page loads for the first time, update the state to reflect the
     // initial url
