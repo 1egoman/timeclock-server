002f6e73b1a39a3112ce79901087be4bd5fe9e42
1egoman <rsg1egoman@gmail.com>
2016-03-08 21:25:43 -0500

    loading component and error handling

--- app.js
+++ app.js
@@ -120,39 +120,32 @@ app.get('/:username/:repo', repo.getRepo, (req, res) => {
   res.redirect(`/app/${req.params.username}/${req.params.repo}`);
 });
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  let err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
 /// error handlers
 
 // development error handler
 // will print stacktrace
 
-// if (app.get('env') === 'development') {
-//   app.use(function(err, req, res, next) {
-//     res.status(err.status || 500);
-//     res.render('error', {
-//       message: err.message,
-//       error: err,
-//       title: 'error'
-//     });
-//   });
-// }
-
 // production error handler
 // no stacktraces leaked to user
-// app.use(function(err, req, res, next) {
-//   res.status(err.status || 500);
-//   res.render('error', {
-//     message: err.message,
-//     error: {},
-//     title: 'error'
-//   });
-// });
+if (process.env.NODE_ENV === "production") {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: {},
+      title: 'error'
+    });
+  });
+} else {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err,
+      title: 'error'
+    });
+  });
+}
 
 // ----------------------------------------------------------------------------
 // socket.io stuff
