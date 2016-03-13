85f278b8cf999cd64cd2e29cdf29a9333779bfe2
1egoman <rsg1egoman@gmail.com>
2016-03-13 11:18:42 -0400

    deploy script for heroku

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-git push heroku master
-heroku run "npm i -g node-sass browserify uglifyjs && bash ./scripts/build.sh" # compile assets
-heroku restart
-echo "deployed!"
+if [ $NODE_ENV == production ]; then
+  npm install -g node-sass browserify uglifyjs
+  bash ./scripts/build.sh # compile assets
+  echo "Assets compiled ad /public/css/style.css and /public/bundle.js"
+fi
