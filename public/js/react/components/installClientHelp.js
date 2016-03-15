69046f2ed43a9438bb81f0b0b0a05ba9848c569f
1egoman <rsg1egoman@gmail.com>
2016-03-15 18:00:44 -0400

    remove footer button infurstructure

--- public/js/react/components/installClientHelp.js
+++ public/js/react/components/installClientHelp.js
@@ -3,8 +3,8 @@ import {connect} from 'react-redux';
 import {Modal, Button} from 'react-bootstrap';
 import {showWaltzInstallInstructions} from '../actions/repo';
 
-export function InstallClientHelpComponent({client_help_dialog, modalState}) {
-  return <Modal show={client_help_dialog} onHide={modalState}>
+export function InstallClientHelpComponent({client_help_dialog, hideModal}) {
+  return <Modal show={client_help_dialog} onHide={hideModal}>
     <Modal.Header closeButton>
       <Modal.Title>Installing the Waltz Client</Modal.Title>
     </Modal.Header>
@@ -14,9 +14,8 @@ export function InstallClientHelpComponent({client_help_dialog, modalState}) {
         dock or taskbar. You will be using the command line via a tool such as
         Terminal.
       </p> 
-
       <h2>Install Node</h2>
-      <p>Waltz is written in node.js. Here's how to install node:</p>
+      <p>Waltz is written in node.js, which is a prerequisite to installing Waltz.</p>
       <h4>Mac</h4>
       <p>
         Go to nodejs.org, click ‘install’, and run through the install process.
@@ -24,17 +23,18 @@ export function InstallClientHelpComponent({client_help_dialog, modalState}) {
       <h4>Ubuntu</h4>
       <p>
         Run the following in a terminal to install node:
-        <pre>curl -sL https://deb.nodesource.com/setup | sudo -E bash -</pre>
-        Then, run this to get the latest nodejs package:
-        <pre>sudo apt-get install -y nodejs</pre>
       </p>
+      <pre>
+        curl -sL https://deb.nodesource.com/setup | sudo -E bash -<br/>
+        sudo apt-get install -y nodejs
+      </pre>
       <h4>Windows</h4>
       <p>Currently, windows support is not supported.</p>
 
       <h2>Install Waltz</h2>
       <p>Open a terminal and paste the following snippet:</p>
       <pre>
-        npm install -g waltz
+        npm install -g waltz<br/>
         waltz --version
       </pre>
     </Modal.Body>
@@ -47,8 +47,8 @@ const InstallClientHelp = connect((store) => {
   };
 }, (dispatch) => {
   return {
-    modalState(state = false) {
-      dispatch(showWaltzInstallInstructions(state));
+    hideModal() {
+      dispatch(showWaltzInstallInstructions(false));
     },
   };
 })(InstallClientHelpComponent);
