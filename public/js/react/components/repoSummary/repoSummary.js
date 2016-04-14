d28a30b7f112ce9fb93e283a44407cb88d2dc065
1egoman <rsg1egoman@gmail.com>
2016-04-14 17:18:39 -0400

    add last timecard info to repo summary

--- public/js/react/components/repoSummary/repoSummary.js
+++ public/js/react/components/repoSummary/repoSummary.js
@@ -1,5 +1,20 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import _ from 'underscore';
+
+// get the most frequemt committer for a repo
+export function getLastCommitter(timecard) {
+  if (timecard && timecard.card && timecard.card.length) {
+    let card = _.last(timecard.card), time;
+    if (time = _.last(card.times)) {
+      return time;
+    } else {
+      return "No committer";
+    }
+  } else {
+    return "No committer";
+  }
+}
 
 export default ({
   timecard,
@@ -7,6 +22,7 @@ export default ({
   color,
   light,
 }) => {
+  let lastCommitter = getLastCommitter(timecard);
   return <div className={`repo-summary ${light ? 'repo-summary-light' : ''}`}>
     <div className="repo-summary-client-info">
       <h1>John Smith</h1>
@@ -16,8 +32,8 @@ export default ({
     </div>
     <div className="repo-summary-committer-info" style={{backgroundColor: color}}>
       <img src="https://avatars3.githubusercontent.com/u/1704236?v=3&s=40" />
-      <h2>Username</h2>
-      <small>1:00pm - 10:00pm</small>
+      <h2>{lastCommitter.by || "No last committer"}</h2>
+      <small>{lastCommitter.start} - {lastCommitter.end}</small>
     </div>
     <div className="repo-summary-graph">
       <div className="repo-summary-no-graph">
