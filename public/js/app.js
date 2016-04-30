f258bbd0878f10a176f3bf79777061fd6f61533d
1egoman <rsg1egoman@gmail.com>
2016-04-30 10:10:41 -0400

    moving animation code to its own file

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,59 @@
+function animate() {
+  var controller = new ScrollMagic.Controller();
+
+  // spreadsheet
+  var scene = new ScrollMagic.Scene({
+    duration: 500,
+    offset: -200,
+    triggerElement: ".spreadsheet-section",
+  })
+  .setTween(".invoice", {
+    transform: 'translate3d(0px, 0, 0)',
+    opacity: 1,
+    offset: -200,
+  })
+  .addTo(controller);
+
+  // version controlled
+  var version = new TimelineMax();
+  version.add(TweenMax.to(".timecard-state.one", 3, {
+    transform: 'translate3d(0px, 0, 0)',
+    width: "29%",
+  }));
+  // fade in all time states
+  version.add(TweenMax.to(".timecard-state", 1, {opacity: 1}));
+
+  var scene = new ScrollMagic.Scene({
+    duration: 500,
+    offset: -100,
+    triggerElement: ".versioned-section",
+  })
+  .setTween(version)
+  .addTo(controller);
+
+  // own your data
+  var scene = new ScrollMagic.Scene({
+    duration: 500,
+    offset: -50,
+    triggerElement: ".owndata-section",
+  })
+  .setTween(".glyph-owndata img", {
+    transform: 'translate3d(-720px, -20px, 0)',
+  })
+  .addTo(controller);
+
+  // terminal
+  var scene = new ScrollMagic.Scene({
+    duration: 500,
+    offset: -50,
+    triggerElement: ".features-tools",
+  })
+  .setTween(".terminal", {
+    height: "25em"
+  })
+  .addTo(controller);
+}
+
 $(document).ready(function () {
   $('[data-toggle="popover"]').popover();
   $('[data-toggle="tooltip"]').tooltip();
@@ -29,4 +85,6 @@ $(document).ready(function () {
   $("[ref=signin]").on("click", function() { mixpanel.track("click-login", {from: "gettingstarted"}) });
   $("[ref=loginnav]").on("click", function() { mixpanel.track("click-login", {from: "nav"}) });
 
+  // animations for home page
+  if (typeof ScrollMagic !== "undefined") animate();
 });
